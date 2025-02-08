22141d126c6e745a2e75cd9bc008d047fb562d16
Peter Fiorella <github@fiorella.me>
2025-02-08 15:31:42 -0700

    Fix logger IP

--- middleware/requestLogger.js
+++ middleware/requestLogger.js
@@ -1,8 +1,9 @@
 const logger = require("../lib/logger");
 
 const requestLogger = (req, res, next) => {
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
   res.on("finish", () => {
-    let request = `${req.method} ${res.statusCode} - ${req.ip} ${req.originalUrl}`;
+    let request = `${req.method} ${res.statusCode} - ${ip} ${req.originalUrl}`;
     logger.info(request);
   });
 
